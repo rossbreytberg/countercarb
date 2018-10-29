2fd4f290fb7ebe1e882dad215e2f47597ef12546
rossbreytberg <rossbreytberg@gmail.com>
2018-10-28 23:30:32 -0700

    added player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    float speed = 1.0f;
+    Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+    void Update () {
+        rb.AddForce(new Vector3(
+            Input.GetAxis("Horizontal") * speed,
+            0.0f,
+            Input.GetAxis("Vertical") * speed
+        ));
+    }
+}
